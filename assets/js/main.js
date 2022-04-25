73f33abbcf5d9473959a762596f42654d7fadc6d
alexisben <alexiben7@gmail.com>
2022-04-25 19:53:22 +0200

    mobile logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,8 @@ import './theme/body';
 import './theme/timeline';
 import FullWidthBlock from './theme/FullWidthBlock';
 
+
+// This is used to set block-page for pleas in full-width inside a smaller col
 if (!document.body.classList.contains('page__home')) {
     const fullWidthBlockElements = document.querySelectorAll('.js-block-full-width');
     fullWidthBlockElements.forEach((element) => {
