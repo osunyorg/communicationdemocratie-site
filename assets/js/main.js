8ffc7d5d5c0765b44739d052a01f893a7b33a7f2
sebousan <sebousan@gmail.com>
2022-05-23 14:22:33 +0200

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 import './vendors/bootstrap';
 import './vendors/lightbox';
 import './vendors/carousel';
-import './theme/body';
+// import './vendors/cookie-banner';
+import './theme';
+// import './theme/body';
 // import './theme/cookie-banner';
-import './theme/timeline';
+// import './theme/timeline';
